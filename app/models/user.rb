14501679f2547a98663ef60ce8862981deac91b5
liweihao <1766141548@qq.com>
2018-11-09 14:25:01 +0800

    临时提交星期五2018.11.9.14.24

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     # 返回指定字符串的哈希摘要
   class << self
 # 返回指定字符串的哈希摘要
