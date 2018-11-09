d3f3056ee343f6869ceef0a8e38956783ed69a59
liweihao <1766141548@qq.com>
2018-11-09 10:34:53 +0800

    Implement advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
   before_save {email.downcase!}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[^.]\.[a-z]+\z/i
@@ -7,9 +8,28 @@ class User < ApplicationRecord
             format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}, presence: true
     # 返回指定字符串的哈希摘要
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-    BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+# 返回指定字符串的哈希摘要
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    # 返回一个随机令牌
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest,User.digest(self.remember_token))
+  end
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  # 忘记用户
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
